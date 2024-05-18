94ee705dd6f807030b1777d2959edbf6f0c3b7d2
Na2-factory <na2.factory.work@gmail.com>
2024-05-18 18:47:24 +0900

    v1.0.4 テーマがCocoon、Cocoonの子テーマの場合のみ動作するように修正

--- grayish-cstmpat-css.php
+++ grayish-cstmpat-css.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: grayish-cstmpat-css
 Description: grayishの1カラム用サンプルパターン向けCSSを追加するプラグイン
-Version: 1.0.3
+Version: 1.0.4
 Author: Na2factory
 Author URI: https://na2-factory.com/
 License: GNU General Public License
@@ -11,8 +11,15 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 if (!defined('ABSPATH')) exit;
 
+// 現在のテーマがcocoon-master or cocoon-masterの子テーマである場合のみ処理を行う
+$theme = wp_get_theme();
+if ('cocoon-master' !== $theme->template && (!$theme->parent() || 'cocoon-master' !== $theme->parent()->template)) {
+	return;
+}
+
+
 if (!defined('GRY_CSTMPAT_PLUGIN_VERSION')) {
-	define('GRY_CSTMPAT_PLUGIN_VERSION', '1.0.3');
+	define('GRY_CSTMPAT_PLUGIN_VERSION', '1.0.4');
 }
 
 if (!defined('GRY_CSTMPAT_PLUGIN_PATH')) {
